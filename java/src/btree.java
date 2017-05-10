d1b3095cdc5a015e326b2a6c5796775f0019369b
Replogle <Dustin.Rep@hke.local>
2017-05-10 09:41:53 -0700

    initial commit

---
+++ java/src/btree.java
@@ -0,0 +1,14 @@
+public class btree {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+        Node hi = new Node();
+        System.out.println(hi);
+    }
+}
+
+class Node {
+    int a = 0;
+    Node() {
+        a = 1;
+    }
+}
