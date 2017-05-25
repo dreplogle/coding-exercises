e89d4edc3dff035333f11aed1084648e546d1cf8
Replogle <Dustin.Rep@hke.local>
2017-05-25 09:14:28 -0700

    Second commit

--- java/src/btree.java
+++ java/src/btree.java
@@ -1,14 +1,30 @@
 public class btree {
     public static void main(String[] args) {
         System.out.println("hello world!");
-        Node hi = new Node();
-        System.out.println(hi);
+        Node root = new Node();
+        BTree tree = new BTree();
+        System.out.println(hi.a);
     }
 }
 
 class Node {
-    int a = 0;
-    Node() {
-        a = 1;
+    int value;
+    Node(int a) {
+        value = a;
     }
+}
+
+
+abstract public class Tree {
+    Node root;
+    Tree() {
+        root = new Node();
+    }
+    void add(int a) {
+        Node temp = new Node(a);
+    }
+}
+
+class BTree extends Tree {
+
 }
